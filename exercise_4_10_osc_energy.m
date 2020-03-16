ce973dd8a30bd26984221dbf07036ad6cf81c429
hopifako <60648787+hopifako@users.noreply.github.com>
2020-03-16 18:32:43 +0200

    Delete exercise_4_10_osc_energy.m

--- exercise_4_10_osc_energy.m
+++
@@ -1,8 +0,0 @@
-%% Exercise 4.10
-%Solution of Exercise 4.10 from S. Lingeand H. P. Langtangen book
-%Function returns the potential and kinetic enegery of an oscillating
-%system 
-function [E_pot , E_kin] = osc_energy(u, v, omega)
-    E_pot = 0.5 * (omega * u).^2;
-    E_kin = 0.5 * v.^2;
-end
